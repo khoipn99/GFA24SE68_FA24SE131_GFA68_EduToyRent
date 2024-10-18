1a84e1f318f8a86de3491240fdce2ccce86a961c
HaiquangPham14 <phamwang02@gmail.com>
2024-10-18 14:54:15 +0700

    add DTO
    add DTO

--- EduToyRent_BE/EduToyRentAPI/obj/Debug/net8.0/EduToyRentAPI.AssemblyInfo.cs
+++ EduToyRent_BE/EduToyRentAPI/obj/Debug/net8.0/EduToyRentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduToyRentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+725d1ee365adb5049d2079d7a26b175834519e74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6bfa682f627ee0559015ab4d0f20ab481b0286b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduToyRentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduToyRentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ EduToyRent_BE/EduToyRentRepositories/DTO/Response/UserResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduToyRentRepositories.DTO.Response
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Phone { get; set; }
+        public DateTime Dob { get; set; }
+        public string Address { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Status { get; set; }
+        public RoleResponse Role { get; set; }
+    }
+}
