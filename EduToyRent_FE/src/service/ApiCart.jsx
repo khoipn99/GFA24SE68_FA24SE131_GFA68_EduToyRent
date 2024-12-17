9fe98188a1a6e7e9e70ee1bc6d98cb7801162101
khoipn99 <135039882+khoipn99@users.noreply.github.com>
2024-12-17 15:38:16 +0700

    Update ApiCart.jsx

--- EduToyRent_FE/src/service/ApiCart.jsx
+++ EduToyRent_FE/src/service/ApiCart.jsx
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 
 const apiCart = axios.create({
   baseURL:
-    "https://edutoyrent-cngbg3hphsg2fdff.southeastasia-01.azurewebsites.net/api/v1/Carts", //3:12
+    "https://edutoyrent-cngbg3hphsg2fdff.southeastasia-01.azurewebsites.net/api/v1/Carts", //3:38
   //baseURL: "https://localhost:44350/api/v1/Carts",
   headers: {
     Authorization: `Bearer ${Cookies.get("userToken")}`,
